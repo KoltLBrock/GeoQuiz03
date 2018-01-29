df44f219056a83299bdf378fae5624ac1e26f74f
KoltLBrock <35542746+KoltLBrock@users.noreply.github.com>
2018-01-28 19:19:37 -0600

    Initial Commit

---
+++ app/src/main/java/com/cidm4385/kolton/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.cidm4385.kolton.geoquiz;
+
+/**
+ * Created by John on 1/28/2018.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
